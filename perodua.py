fc3f1819be367c9b6d01e8ea0c48f1c27169c293
FikriAiman04 <fikrizarin04@gmail.com>
2025-06-04 01:06:10 +0800

    Update perodua.py

--- perodua.py
+++ perodua.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 import base64
 
-def set_background_scaled(jpg_file, size="50%"):
+def set_background_scaled(jpg_file, size="80%"):
     with open(jpg_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode()
 
