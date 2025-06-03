bfb8dfb8986af3ed59e0e0a5cafa5c7f5ae4dcb1
FikriAiman04 <fikrizarin04@gmail.com>
2025-06-03 23:04:18 +0800

    Update perodua.py

--- perodua.py
+++ perodua.py
@@ -2,13 +2,14 @@ import streamlit as st
 
 # Data model Perodua & gambar (dari laman rasmi)
 perodua_data = {
-    "Axia": "https://www.perodua.com.my/images/car-model/axia.png",
-    "Bezza": "https://www.perodua.com.my/images/car-model/bezza.png",
-    "Myvi": "https://www.perodua.com.my/images/car-model/myvi.png",
-    "Ativa": "https://www.perodua.com.my/images/car-model/ativa.png",
-    "Alza": "https://www.perodua.com.my/images/car-model/alza.png",
-    "Aruz": "https://www.perodua.com.my/images/car-model/aruz.png",
+    "Axia": "https://upload.wikimedia.org/wikipedia/commons/8/8b/Perodua_Axia_1.0_SE_(2014)_in_JB_Malaysia_(cropped).jpg",
+    "Bezza": "https://upload.wikimedia.org/wikipedia/commons/4/42/Perodua_Bezza_1.3_Advance_(2016)_in_JB_Malaysia.jpg",
+    "Myvi": "https://upload.wikimedia.org/wikipedia/commons/a/a1/Perodua_Myvi_1.5_AV_facelift_2021.jpg",
+    "Ativa": "https://upload.wikimedia.org/wikipedia/commons/e/e2/2021_Perodua_Ativa_1.0_AV.jpg",
+    "Alza": "https://upload.wikimedia.org/wikipedia/commons/8/8b/2022_Perodua_Alza_1.5_AV.jpg",
+    "Aruz": "https://upload.wikimedia.org/wikipedia/commons/9/91/2019_Perodua_Aruz_1.5_AV.jpg",
 }
+
 
 # Tetapan Streamlit
 st.set_page_config(page_title="Perodua Car Search", page_icon="🚗")
